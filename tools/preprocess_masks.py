12bb14fd9676ab4cae7b1707839dfd0b2d9552aa
yeasar <syeasar.kuet@gmail.com>
2025-06-24 13:28:45 +0600

    :up: update: preprocess_masks

--- tools/preprocess_masks.py
+++ tools/preprocess_masks.py
@@ -12,8 +12,9 @@ COLOR_CLASS_MAP = {
     (255, 0, 0): 4,
     (255, 0, 255): 5,
     (255, 255, 0): 6,
-    (255, 255, 255): 7
+    (255, 255, 255): 7,
 }
+
 
 def convert_mask(input_path, output_path):
     mask = Image.open(input_path).convert("RGB")
@@ -21,10 +22,17 @@ def convert_mask(input_path, output_path):
     h, w, _ = arr.shape
     result = np.zeros((h, w), dtype=np.uint8)
 
+    mapped = np.zeros((h, w), dtype=bool)
     for rgb, idx in COLOR_CLASS_MAP.items():
-        result[np.all(arr == rgb, axis=-1)] = idx
+        match = np.all(arr == rgb, axis=-1)
+        result[match] = idx
+        mapped = mapped | match
+
+    if not np.all(mapped):
+        print(f"Warning: {input_path} has unmapped colors!")
 
     Image.fromarray(result).save(output_path)
+
 
 input_dir = "data/SUIM/masks_rgb/train"
 output_dir = "data/SUIM/masks_indexed/train"
@@ -35,4 +43,3 @@ for file in os.listdir(input_dir):
         in_path = os.path.join(input_dir, file)
         out_path = os.path.join(output_dir, file.replace(".bmp", ".png"))
         convert_mask(in_path, out_path)
-
